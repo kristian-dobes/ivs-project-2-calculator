243a7135c2f5039be050406ed7523eab71dd0788
kristian-dobes <101816479+kristian-dobes@users.noreply.github.com>
2022-04-15 12:27:04 +0200

    Add files via upload

--- src/mathlibrary.py
+++ src/mathlibrary.py
@@ -1,60 +1,47 @@
 #math-library
 
 
-DividingByZero=0            
-InvalidFactorial=0
-
-
-def sum(num1, num2):
+def sum(num1, num2):                #Summation
     return num1+num2
 
-def sub(num1, num2):
+
+def sub(num1, num2):                #Subtraction
     return num1-num2
 
-def mult(num1, num2):
+
+def mult(num1, num2):               #Multiplication
     return num1*num2
 
-def div(divident, divisor):
+
+def div(divident, divisor):         #Division
     if divisor==0:
-        print("Invalid: dividing by zero")
-        global DividingByZero             #not ideal
-        DividingByZero=1
+        raise ZeroDivisionError("Invalid: Divisor cannot be 0")    
     else:
         result=divident/divisor
         return round(result,7)
     
-def Pow(basis, exponent):       #CAPITAL Pow
+    
+def Pow(basis, exponent):            #Exponentiation    #CAPITAL Pow
     result = pow(basis, exponent)
     return round(result,7)
 
-def nroot(num, root):
+
+def nroot(num, root):                #Nth-Root
     if root==0: 
-        print("Invalid: dividing by zero")
-        global DividingByZero             #not ideal
-        DividingByZero=1
+        raise ZeroDivisionError("Invalid: Divisor cannot be 0")    
     else:
         result = num**(1/root)
         return result
 
-def rem(divident, divisor):
-    if divisor==0:        
-        print("Invalid: dividing by zero")
-        global DividingByZero             #not ideal
-        DividingByZero=1        
-    else:
-        return divident%divisor
 
-def fac(num): 
+def fac(num):                        #Factorial
     factorial = 1
     if num<0 or num%1 != 0:
-        print("Invalid: invalid factorial argument")
-        global InvalidFactorial
-        InvalidFactorial=1
-        return 
+        raise ValueError("Value need to be a non-negative integer")
     elif num==0:
         result=1
-    # elif num>170:
-        
+    elif num>170:
+        raise OverflowError("Factorial above 170")
     else:
         for i in range(1,num + 1):         
             factorial = factorial*i
@@ -63,11 +50,8 @@ def fac(num):
     return result
 
 
-
-#result=div(4,0)
-result=fac(20)
-
-print("The result is:",result)
-print("Dividing by zero value:",DividingByZero)
-print("Invalid Factorial Value:",InvalidFactorial)
-
+def rem(divident, divisor):          #Division Remainder
+    if divisor==0 or divisor=={}:        
+        raise ZeroDivisionError("Invalid: Divisor cannot be 0")        
+    else:
+        return divident%divisor
