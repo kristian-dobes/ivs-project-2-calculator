839b6bff58c434d24d604b44e9e456070fef3b67
balek01 <mirbalek@seznam.cz>
2022-04-21 16:34:48 +0200

    Update mathlibrary.py

--- src/mathlibrary.py
+++ src/mathlibrary.py
@@ -1,57 +1,102 @@
-#math-library
+#############################################################################################################
+# Název projektu: Matematická kalkulačka
+# Soubor: mathlibrary.py
+# Datum: 21.4.2022
+# Poslední změna: 22.4.2022
+# Autoř:Miroslav Bálek (xbalek02), Kristián Dobeš (xdobes22), Maroš Synák (xsynak03), Jan Král (xkralj12)
+#
+# Popis: Knihovna, která vykonává základní matematické operace jako sčítání, odčítání, násobení, dělení,
+# celočíselný zbytek po dělení, umocnění, obecnou odmocninu a faktoriál 
+#############################################################################################################
+
+##
+# @file mathlibrary.py
+#
+# @brief Matematická knihovna
+#
+# @author Miroslav Bálek (xbalek02), Kristián Dobeš (xdobes22), Maroš Synák (xsynak03), Jan Král (xkralj12)
+# @version 1.0
+#
+# @section DESCRIPTION
+#
+# Knihovna, která vykonává základní matematické operace jako sčítání, odčítání, násobení, dělení,
+# celočíselný zbytek po dělení, umocnění, obecnou odmocninu a faktoriál.
+#  Všechny operace jsou počítány s přesností na sedm desetinných míst.
 
 
-def sum(num1, num2):                #Summation
+
+##
+# Funkce pro sečtení dvou čísel 
+# @param num1 první číslo
+# @param num1 druhé číslo 
+#
+# @return Součet parametrů
+def sum(num1, num2):  # Summation
     return num1+num2
 
-
-def sub(num1, num2):                #Subtraction
+##
+# Funkce pro výpočet rozdílu dvou čísel 
+# @param num1 první číslo
+# @param num1 druhé číslo 
+#
+# @return Rozdíl parametrů
+def sub(num1, num2):  # Subtraction
     return num1-num2
 
-
-def mult(num1, num2):               #Multiplication
+##
+# Funkce pro výpočet součinu dvou čísel 
+# @param num1 první číslo
+# @param num1 druhé číslo 
+#
+# @return Součin parametrů
+def mult(num1, num2):  # Multiplication
     return num1*num2
-
-
-def div(divident, divisor):         #Division
-    if divisor==0:
-        raise ZeroDivisionError("Invalid: Divisor cannot be 0")    
+##
+# Funkce pro výpočet součinu dvou čísel 
+# @param divident dělenec
+# @param divisor dělitel
+#
+# @return Podíl dělenec/dělitel
+def div(divident, divisor):  # Division
+    if divisor == 0:
+        raise ZeroDivisionError("Invalid: Divisor cannot be 0")
     else:
         return divident/divisor
-    
-    
-def Pow(basis, exponent):            #Exponentiation    #CAPITAL Pow
+
+
+def Pow(basis, exponent):  # Exponentiation    #CAPITAL Pow
     result = pow(basis, exponent)
     return result
 
 
-def nroot(num, root):                #Nth-Root
-    if root==0: 
-        raise ZeroDivisionError("Invalid: Divisor cannot be 0")    
+def nroot(num, root):  # Nth-Root
+    if root == 0:
+        raise ZeroDivisionError("Invalid: Divisor cannot be 0")
     else:
         result = num**(1/root)
         return result
 
 
-def fac(num):                        #Factorial
+def fac(num):  # Factorial
     factorial = 1
-    if num<0 or num%1 != 0:
+    if num < 0 or num % 1 != 0:
         raise ValueError("Value needs to be a non-negative integer")
-    elif num==0:
-        result=1
-    elif num>170:
+    elif num == 0:
+        result = 1
+    elif num > 170:
         raise OverflowError("Factorial above 170")
     else:
-        for i in range(1,num + 1):         
+        for i in range(1, num + 1):
             factorial = factorial*i
             result = factorial
 
     # return ("{:e}".format(result))        #returns factorial as Scientific Notation
-    return result                           #returns factorial as Integer
+    return result  # returns factorial as Integer
 
 
-def rem(divident, divisor):          #Division Remainder
-    if divisor==0 or divisor=={}:        
-        raise ZeroDivisionError("Invalid: Divisor cannot be 0")        
+def rem(divident, divisor):  # Division Remainder
+    if divisor == 0 or divisor == {}:
+        raise ZeroDivisionError("Invalid: Divisor cannot be 0")
     else:
-        return divident%divisor
+        return divident % divisor
+    
