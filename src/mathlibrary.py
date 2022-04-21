7b64d3a0e32c8af62784f64090e7468e72b0879a
kristian-dobes <101816479+kristian-dobes@users.noreply.github.com>
2022-04-21 15:30:26 +0200

    changed function names

--- src/mathlibrary.py
+++ src/mathlibrary.py
@@ -20,12 +20,12 @@ def div(divident, divisor):         #Division
         return divident/divisor
     
     
-def Pow(basis, exponent):            #Exponentiation    #CAPITAL Pow
+def mypow(basis, exponent):            #Exponentiation
     result = pow(basis, exponent)
     return result
 
 
-def nroot(num, root):                #Nth-Root
+def root(num, root):                #Nth-Root
     if root==0: 
         raise ZeroDivisionError("Invalid: Divisor cannot be 0")    
     else:
