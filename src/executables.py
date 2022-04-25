070bec914663109bb080dcf05494406c010053b2
Marosmobil <101818775+Marosmobil@users.noreply.github.com>
2022-04-25 19:17:42 +0200

    Add files via upload

--- src/executables.py
+++ src/executables.py
@@ -1,241 +1,124 @@
+#############################################################################################################
+# Název projektu: Matematická kalkulačka
+# Soubor: mathlibrary.py
+# Datum: 22.4.2022
+# Poslední změna: 24.4.2022
+# Autoři: Miroslav Bálek (xbalek02), Kristián Dobeš (xdobes22), Maroš Synák (xsynak03), Jan Král (xkralj12)
+#
+# Modul for integrating math library with gui, takes inputs from gui uses math library to calculate results
+# and delivers them back to gui
+#############################################################################################################
+
 ##
 # @file executables.py
 #
-# @brief Modul pro výpis do GUI a pro volání funkcí z matematické knihovny. 
+# @brief Matematická knihovna
 #
 # @author Miroslav Bálek (xbalek02), Kristián Dobeš (xdobes22), Maroš Synák (xsynak03), Jan Král (xkralj12)
 # @version 1.0
 #
 # @section DESCRIPTION
 #
-# Modul slouží k výpisu do GUI. Po zadání inputu rozlišuje zadané operace a zajišťuje volání správných funkcí z matematické knihovny.
-# 
+# Modul for integrating math library with gui, takes inputs from gui uses math library to calculate results
+# and delivers them back to gui
 
 
 import gui
 import tkinter
-from sympy import pretty_print
-from sympy.abc import a, b, n
 from tkinter.tix import *
 import math_library
 
+ex = 0
 ##
-# @brief Funkce pro vypsání čísla 1 do GUI okna 
-# 
-def print_1(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"1")
-    gui.display.configure(state='disabled')
+# Funkce pro vypsání odmocniny do displeje, je separatne kvůli použití speciálního znaku
+# @param ignore mě být ignorován, je tam proto, že bind potřebuje funkce s parametrem
 
-##
-# @brief Funkce pro vypsání čísla 2 do GUI okna 
-# 
-def print_2(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"2")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání čísla 3 do GUI okna 
-# 
-def print_3(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"3")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání čísla 4 do GUI okna 
-# 
-def print_4(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"4")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání čísla 5 do GUI okna 
-# 
-def print_5(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"5")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání čísla 6 do GUI okna 
-# 
-def print_6(ignore=0):
-
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"6")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání čísla 7 do GUI okna 
-# 
-def print_7(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"7")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání čísla 8 do GUI okna 
-# 
-def print_8(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"8")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání čísla 9 do GUI okna 
-# 
-def print_9(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"9")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání čísla 0 do GUI okna 
-# 
-def print_0(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"0")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání znaku + do GUI okna 
-# 
-def print_plus(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"+")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání znaku - do GUI okna 
-#     
-def print_minus(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"-")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání znaku * do GUI okna 
-# 
-def print_mul(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"*")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání znaku / do GUI okna 
-# 
-def print_div(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"/")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání znaku ^ do GUI okna 
-# 
-def print_n_pow(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"^")
-    gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro vypsání znaku √ do GUI okna 
-# 
 def print_n_root(ignore=0):
+    global ex
     gui.display.configure(state='normal')
     gui.display.insert(END,"√")
     gui.display.configure(state='disabled')
 
-
-#def print_lp(ignore=0):
- #   display.configure(state='normal')
-#    display.insert(END,"(")
-#    display.configure(state='disabled')
-#def print_rp(ignore=0):
-#    display.configure(state='normal')
-#    display.insert(END,")")
-#    display.configure(state='disabled')
-
 ##
-# @brief Funkce pro vypsání znaku .\ do GUI okna 
-# 
-def print_dot():
+# Funkce pro vypsání všech potřebných znaků kromě odmocniny do displeje
+# @param ignore num představuje znak, který je třeba vypsat
+
+def print_num(num):
+    global ex
+    if ex == 1:
+        gui.display.delete("1.0",END)
+        ex = 0
     gui.display.configure(state='normal')
-    gui.display.insert(END,".")
-    gui.display.configure(state='disabled')
-  
-##
-# @brief Funkce pro vypsání znaku ! do GUI okna 
-# 
-def print_fac(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"!")
+    gui.display.insert(END,num)
     gui.display.configure(state='disabled')
 
 ##
-# @brief Funkce pro vypsání znaku % do GUI okna 
-#
-def print_mod(ignore=0):
-    gui.display.configure(state='normal')
-    gui.display.insert(END,"%")
-    gui.display.configure(state='disabled')
+# Funkce která načítá vstupy z displeje, následně vypočítává a vrací výsledek do displeje
 
-##
-# @brief Funkce pro volání funkcí z knihovny mathlibrary.\ py
-#
 def execute():
+    global ex
+    if ex == 1:
+        gui.display.delete("1.0",END)
+        ex = 0
     gui.display.configure(state = 'normal')
     equation = gui.display.get("1.0",END)
-    #print(equation)
     num = ""
     appended_sign = 0
     arr_inputs = []
     ex = 0
     minus_num=0
-    for i in equation: #prochází input znak po znaku
-        if i in ('1','2','3','4','5','6','7','8','9','0'): #pokud obsahuje číslici 
+    appended_sign_minus=0
+    for i in equation:
+        if i in ('1','2','3','4','5','6','7','8','9','0'):
             if (appended_sign == 2):
                 gui.display.delete("1.0",END)
                 gui.display.insert("1.0","Synatax error")
                 ex = 1
+            appended_sign_minus=0
             appended_sign = 0
             if (minus_num == 1):
                 num += '-'
                 minus_num=0
             num += i
         else:
-            if appended_sign == 1:
+            if(i == '-'):
+                appended_sign_minus=appended_sign+1
+            print(appended_sign, appended_sign_minus)
+            if (appended_sign == 1 and not (appended_sign_minus == 1 or appended_sign_minus == 2 )):
                 gui.display.delete("1.0",END)
                 gui.display.insert("1.0","Synatax error")
                 ex = 1
-            if num != "":
+            if num != "" and i != '.':
                 arr_inputs.append(num)
-            if i != '\n' and num=="" and appended_sign==0:
+            elif num != "" and i == '.':
+                num += i
+            if i == '-' and num=="" and appended_sign==0:
+                print("ggg")
                 minus_num=1
                 continue
-            elif i != '\n':
+            elif i != '\n' and appended_sign_minus != 2 and i !='.':
                 arr_inputs.append(i)
                 if (i != '!'):
                     appended_sign=1
                 else:
                     appended_sign=2
-            num=""
+            if i != '.':
+                num=""
+            if appended_sign_minus == 2:
+                num+='-'
 
     if ex == 1:
         return
     #factorial
+    for i in range(0,len(arr_inputs)):
+        print(arr_inputs[i])
     to_delete = []
-    length = len(arr_inputs)                              #!nevyužitá proměná? 
-    for i in range(0,len(arr_inputs)):                    #prochází pole znak po znaku
-        if arr_inputs[i] == '!':                          #pokud prvek pole obsahuje "operaci"
-            to_delete.append(i)                           #přidá danou operaci do array pro smazání 
-            number = int(arr_inputs[i-1])                 #nastaví number na hodnotu čísel před operací
-            arr_inputs[i-1] = math_library.fac(number)    #zapíše na pozici čísla v poli výsledek operace
-            #for o in range(i+1,len(arr_inputs)):
-               # arr_inputs[o-1]=arr_inputs[o]
-    for i in reversed(to_delete):                         #odstranění přebytečných znaků z pole
+    for i in range(0,len(arr_inputs)):
+        if arr_inputs[i] == '!':
+            to_delete.append(i)
+            number = int(arr_inputs[i-1])
+            arr_inputs[i-1] = math_library.fac(number)
+    for i in reversed(to_delete):
         arr_inputs.pop(i)
     to_delete = []
     
@@ -244,8 +127,8 @@ def execute():
         if arr_inputs[i] == '√':
             to_delete.append(i)
             to_delete.append(i+1)
-            number1 = int(arr_inputs[i-1])
-            number2 = int(arr_inputs[i+1])
+            number1 = float(arr_inputs[i-1])
+            number2 = float(arr_inputs[i+1])
             arr_inputs[i-1] = math_library.root(number2,number1)
     for i in reversed(to_delete):
         arr_inputs.pop(i)
@@ -256,8 +139,8 @@ def execute():
         if arr_inputs[i] == '^':
             to_delete.append(i)
             to_delete.append(i+1)
-            number1 = int(arr_inputs[i-1])
-            number2 = int(arr_inputs[i+1])
+            number1 = float(arr_inputs[i-1])
+            number2 = float(arr_inputs[i+1])
             arr_inputs[i-1] = math_library.mypow(number1,number2)
     for i in reversed(to_delete):
         arr_inputs.pop(i)
@@ -268,8 +151,8 @@ def execute():
         if arr_inputs[i] == '/':
             to_delete.append(i)
             to_delete.append(i+1)
-            number1 = int(arr_inputs[i-1])
-            number2 = int(arr_inputs[i+1])
+            number1 = float(arr_inputs[i-1])
+            number2 = float(arr_inputs[i+1])
             arr_inputs[i-1] = math_library.div(number1,number2)
     for i in reversed(to_delete):
         arr_inputs.pop(i)
@@ -280,8 +163,8 @@ def execute():
         if arr_inputs[i] == '/':
             to_delete.append(i)
             to_delete.append(i+1)
-            number1 = int(arr_inputs[i-1])
-            number2 = int(arr_inputs[i+1])
+            number1 = float(arr_inputs[i-1])
+            number2 = float(arr_inputs[i+1])
             arr_inputs[i-1] = math_library.rem(number1,number2)
     for i in reversed(to_delete):
         arr_inputs.pop(i)
@@ -291,8 +174,8 @@ def execute():
         if arr_inputs[i] == '*':
             to_delete.append(i)
             to_delete.append(i+1)
-            number1 = int(arr_inputs[i-1])
-            number2 = int(arr_inputs[i+1])
+            number1 = float(arr_inputs[i-1])
+            number2 = float(arr_inputs[i+1])
             arr_inputs[i-1] = math_library.mult(number1,number2)
     for i in reversed(to_delete):
         arr_inputs.pop(i)
@@ -303,8 +186,8 @@ def execute():
         if arr_inputs[i] == '-':
             to_delete.append(i)
             to_delete.append(i+1)
-            number1 = int(arr_inputs[i-1])
-            number2 = int(arr_inputs[i+1])
+            number1 = float(arr_inputs[i-1])
+            number2 = float(arr_inputs[i+1])
             arr_inputs[i-1] = math_library.sub(number1,number2)
     for i in reversed(to_delete):
         arr_inputs.pop(i)
@@ -315,8 +198,8 @@ def execute():
         if arr_inputs[i] == '+':
             to_delete.append(i)
             to_delete.append(i+1)
-            number1 = int(arr_inputs[i-1])
-            number2 = int(arr_inputs[i+1])
+            number1 = float(arr_inputs[i-1])
+            number2 = float(arr_inputs[i+1])
             arr_inputs[i-1] = math_library.sum(number1,number2)
     for i in reversed(to_delete):
         arr_inputs.pop(i)
@@ -325,18 +208,11 @@ def execute():
     gui.display.delete("1.0",END)
     gui.display.insert("1.0",str(arr_inputs[0]))
     gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro smazání posledního znaku GUI okna
-#
+        
 def del_last_pos(ignore=0):
     gui.display.configure(state='normal')
     gui.display.delete("end-2c")
     gui.display.configure(state='disabled')
-
-##
-# @brief Funkce pro smazání obsahu GUI okna
-# 
 def del_all():
     gui.display.configure(state='normal')
     gui.display.delete("1.0",END)
