baab4f2098b88a623eccf207a80d6282607c2638
Marosmobil <101818775+Marosmobil@users.noreply.github.com>
2022-04-27 15:33:40 +0200

    Add files via upload

--- src/executables.py
+++ src/executables.py
@@ -1,6 +1,6 @@
 #############################################################################################################
 # Název projektu: Matematická kalkulačka
-# Soubor: executables.py
+# Soubor: mathlibrary.py
 # Datum: 22.4.2022
 # Poslední změna: 24.4.2022
 # Autoři: Miroslav Bálek (xbalek02), Kristián Dobeš (xdobes22), Maroš Synák (xsynak03), Jan Král (xkralj12)
@@ -25,7 +25,7 @@
 
 import gui
 import tkinter
-from tkinter.tix import *
+#from tkinter.tix import *
 import mathlibrary
 
 ex = 0
@@ -36,7 +36,7 @@ ex = 0
 def print_n_root(ignore=0):
     global ex
     gui.display.configure(state='normal')
-    gui.display.insert(END,"√")
+    gui.display.insert(tkinter.END,"√")
     gui.display.configure(state='disabled')
 
 ##
@@ -46,10 +46,10 @@ def print_n_root(ignore=0):
 def print_num(num):
     global ex
     if ex == 1:
-        gui.display.delete("1.0",END)
+        gui.display.delete("1.0",tkinter.END)
         ex = 0
     gui.display.configure(state='normal')
-    gui.display.insert(END,num)
+    gui.display.insert(tkinter.END,num)
     gui.display.configure(state='disabled')
 
 ##
@@ -58,10 +58,10 @@ def print_num(num):
 def execute():
     global ex
     if ex == 1:
-        gui.display.delete("1.0",END)
+        gui.display.delete("1.0",tkinter.END)
         ex = 0
     gui.display.configure(state = 'normal')
-    equation = gui.display.get("1.0",END)
+    equation = gui.display.get("1.0",tkinter.END)
     num = ""
     appended_sign = 0
     arr_inputs = []
@@ -71,7 +71,7 @@ def execute():
     for i in equation:
         if i in ('1','2','3','4','5','6','7','8','9','0'):
             if (appended_sign == 2):
-                gui.display.delete("1.0",END)
+                gui.display.delete("1.0",tkinter.END)
                 gui.display.insert("1.0","Synatax error")
                 ex = 1
             appended_sign_minus=0
@@ -85,7 +85,7 @@ def execute():
                 appended_sign_minus=appended_sign+1
             print(appended_sign, appended_sign_minus)
             if (appended_sign == 1 and not (appended_sign_minus == 1 or appended_sign_minus == 2 )):
-                gui.display.delete("1.0",END)
+                gui.display.delete("1.0",tkinter.END)
                 gui.display.insert("1.0","Synatax error")
                 ex = 1
             if num != "" and i != '.':
@@ -205,7 +205,7 @@ def execute():
         arr_inputs.pop(i)
     to_delete = []
 
-    gui.display.delete("1.0",END)
+    gui.display.delete("1.0",tkinter.END)
     gui.display.insert("1.0",str(arr_inputs[0]))
     gui.display.configure(state='disabled')
         
@@ -215,5 +215,80 @@ def del_last_pos(ignore=0):
     gui.display.configure(state='disabled')
 def del_all():
     gui.display.configure(state='normal')
-    gui.display.delete("1.0",END)
+    gui.display.delete("1.0",tkinter.END)
     gui.display.configure(state='disabled')
+
+def help_in():
+    gui.num1.place_forget()
+    gui.num2.place_forget()
+    gui.num3.place_forget()
+    gui.num4.place_forget()
+    gui.num5.place_forget()
+    gui.num6.place_forget()
+    gui.num7.place_forget()
+    gui.num8.place_forget()
+    gui.num9.place_forget()
+    gui.num0.place_forget()
+    gui.plus.place_forget()
+    gui.minus.place_forget()
+    gui.times.place_forget()
+    gui.division.place_forget()
+    gui.fac.place_forget()
+    gui.root.place_forget()
+    gui.tpow.place_forget()
+    gui.delete.place_forget()
+    gui.ce.place_forget()
+    gui.float_point.place_forget()
+    gui.mod.place_forget()
+    gui.equals.place_forget()
+    gui.display.place_forget()
+    gui.help_in.place_forget()
+
+    gui.help_out.place(x=0,y=0)
+    #text = gui.canvas.create_text(5,50,text="+", anchor=tkinter.W)
+    text = gui.canvas.create_text(5,50,text= "+ :adds 2 numbers format(num1+num2)", anchor=tkinter.W)
+    #text = gui.canvas.create_text(5,100,text= "-")
+    text = gui.canvas.create_text(5,75,text= "- :substracts 2 numbers format(num1-num2)", anchor=tkinter.W)
+    #text = gui.canvas.create_text(5,150,text= "/", anchor=tkinter.W)
+    text = gui.canvas.create_text(5,100,text= "/ :divides 2 numbers format(num1/num2)", anchor=tkinter.W)
+   # text = gui.canvas.create_text(5,200,text= "*", anchor=tkinter.W)
+    text = gui.canvas.create_text(5,125,text= "* :multiplies 2 numbers format(num1*num2)", anchor=tkinter.W)
+    #text = gui.canvas.create_text(5,250,text= "%", anchor=tkinter.W)
+    text = gui.canvas.create_text(5,150,text= "x^n :num x to the power of num2 format(num1^num2)", anchor=tkinter.W)
+    text = gui.canvas.create_text(5,175,text= "n! :factorial of n format(n!)", anchor=tkinter.W)
+    text = gui.canvas.create_text(5,200,text= "n√x :nth root of x format(n√x)", anchor=tkinter.W)
+    text = gui.canvas.create_text(5,225,text= ". :floating point (whole_num.float_num)", anchor=tkinter.W)
+   
+    gui.canvas.pack()
+
+def help_out():
+    gui.display.place(x=19,y=30)
+    gui.num1.place(x=20,y=360)
+    gui.num4.place(x=20,y=310)
+    gui.num7.place(x=20,y=260)
+    gui.num2.place(x=90,y=360)
+    gui.num5.place(x=90,y=310)
+    gui.num8.place(x=90,y=260)
+    gui.num3.place(x=160,y=360)
+    gui.num6.place(x=160,y=310)
+    gui.num9.place(x=160,y=260)
+
+    gui.fac.place(x=20,y=210)
+    gui.root.place(x=90,y=210)
+    gui.tpow.place(x=160,y=210)
+    gui.mod.place(x=160,y=410)
+
+    gui.plus.place(x=300,y=310)
+    gui.minus.place(x=230,y=310)
+    gui.times.place(x=230,y=260)
+
+    gui.num0.place(x=20,y=410)
+    gui.float_point.place(x=90,y=410)
+    gui.delete.place(x=230,y=210)
+    gui.ce.place(x=300,y=210)
+    gui.equals.place(x=230,y=362)
+    gui.division.place(x=300,y=260)
+    gui.help_out.place_forget()
+    gui.canvas.pack_forget()
+    gui.help_in.place(x=0,y=0)
+
--- src/gui.py
+++ src/gui.py
@@ -1,6 +1,11 @@
+import tkinter
+#from tkinter.tix import *
+import executables
+
+win = tkinter.Tk()
 #############################################################################################################
 # Název projektu: Matematická kalkulačka
-# Soubor: gui.py
+# Soubor: mathlibrary.py
 # Datum: 10.4.2022
 # Poslední změna: 24.4.2022
 # Autoři: Miroslav Bálek (xbalek02), Kristián Dobeš (xdobes22), Maroš Synák (xsynak03), Jan Král (xkralj12)
@@ -10,7 +15,7 @@
 #############################################################################################################
 
 ##
-# @file gui.py
+# @file guy.py
 #
 # @brief Matematická knihovna
 #
@@ -22,105 +27,103 @@
 # modul pro grafické uživatelské rozhraní včetně všech čísel a symbolů nezbytných pro matematickou knihovnu
 # a jejich zobrazení
 
-import tkinter
-from tkinter.tix import *
-import executables
-
-win = Tk()
-
 # creating base for calculator
-win.geometry("390x460")
+win.geometry("390x470")
 win.title("Calculator")
 canvas = tkinter.Canvas(win)
 
 # creating tooltips
-tooltip_plus  = Balloon(win)
-tooltip_minus = Balloon(win)
-tooltip_mul   = Balloon(win)
-tooltip_div   = Balloon(win)
-tooltip_pow   = Balloon(win)
-tooltip_root  = Balloon(win)
-tooltip_del   = Balloon(win)
-tooltip_ce    = Balloon(win)
-tooltip_fac   = Balloon(win)
+#tooltip_plus  = Balloon(win)
+#tooltip_minus = Balloon(win)
+#tooltip_mul   = Balloon(win)
+#tooltip_div   = Balloon(win)
+#tooltip_pow   = Balloon(win)
+#tooltip_root  = Balloon(win)
+#tooltip_del   = Balloon(win)
+#tooltip_ce    = Balloon(win)
+#tooltip_fac   = Balloon(win)
 
 # creating display
 display = tkinter.Text(width = 43,height=10)
 display.configure(state='disabled')
 
+#creating help button
+help_in = tkinter.Button(win,text="?",command=lambda: executables.help_in(),width=1,height=1)
+help_out = tkinter.Button(win,text="?",command=lambda: executables.help_out(),width=1,height=1)
 
 #creating buttons
     
-num1 = tkinter.Button(win,text="1",command=lambda: executables.print_num('1'),width=8,height=2)
-num2 = tkinter.Button(win,text="2",command=lambda: executables.print_num('2'),width=8,height=2)
-num3 = tkinter.Button(win,text="3",command=lambda: executables.print_num('3'),width=8,height=2)
-num4 = tkinter.Button(win,text="4",command=lambda: executables.print_num('4'),width=8,height=2)
-num5 = tkinter.Button(win,text="5",command=lambda: executables.print_num('5'),width=8,height=2)
-num6 = tkinter.Button(win,text="6",command=lambda: executables.print_num('6'),width=8,height=2)
-num7 = tkinter.Button(win,text="7",command=lambda: executables.print_num('7'),width=8,height=2)
-num8 = tkinter.Button(win,text="8",command=lambda: executables.print_num('8'),width=8,height=2)
-num9 = tkinter.Button(win,text="9",command=lambda: executables.print_num('9'),width=8,height=2)
-num0 = tkinter.Button(win,text="0",command=lambda: executables.print_num('0'),width=8,height=2)
+num1 = tkinter.Button(win,text="1",command=lambda: executables.print_num('1'),width=5,height=2)
+num2 = tkinter.Button(win,text="2",command=lambda: executables.print_num('2'),width=5,height=2)
+num3 = tkinter.Button(win,text="3",command=lambda: executables.print_num('3'),width=5,height=2)
+num4 = tkinter.Button(win,text="4",command=lambda: executables.print_num('4'),width=5,height=2)
+num5 = tkinter.Button(win,text="5",command=lambda: executables.print_num('5'),width=5,height=2)
+num6 = tkinter.Button(win,text="6",command=lambda: executables.print_num('6'),width=5,height=2)
+num7 = tkinter.Button(win,text="7",command=lambda: executables.print_num('7'),width=5,height=2)
+num8 = tkinter.Button(win,text="8",command=lambda: executables.print_num('8'),width=5,height=2)
+num9 = tkinter.Button(win,text="9",command=lambda: executables.print_num('9'),width=5,height=2)
+num0 = tkinter.Button(win,text="0",command=lambda: executables.print_num('0'),width=5,height=2)
 
-plus = tkinter.Button(win,text="+",command=lambda: executables.print_num('+'),width=8,height=2)
-minus = tkinter.Button(win,text="-",command=lambda: executables.print_num('-'),width=8,height=2)
-times = tkinter.Button(win,text="*",command=lambda: executables.print_num('*'),width=8,height=2)
-division = tkinter.Button(win,text="÷",command=lambda: executables.print_num('/'),width=8,height=2)
+plus = tkinter.Button(win,text="+",command=lambda: executables.print_num('+'),width=5,height=2)
+minus = tkinter.Button(win,text="-",command=lambda: executables.print_num('-'),width=5,height=2)
+times = tkinter.Button(win,text="*",command=lambda: executables.print_num('*'),width=5,height=2)
+division = tkinter.Button(win,text="÷",command=lambda: executables.print_num('/'),width=5,height=2)
 
-float_point = tkinter.Button(win,text=".",command=lambda: executables.print_num('.'),width=8,height=2)
+float_point = tkinter.Button(win,text=".",command=lambda: executables.print_num('.'),width=5,height=2)
 
-ce = tkinter.Button(win,text="CE",command=executables.del_all,width=8,height=2)
-delete = tkinter.Button(win,text="⌫",command=executables.del_last_pos,width=8,height=2)
+ce = tkinter.Button(win,text="CE",command=executables.del_all,width=5,height=2)
+delete = tkinter.Button(win,text="⌫",command=executables.del_last_pos,width=5,height=2)
 
-fac = tkinter.Button(win,text="n!",command=lambda: executables.print_num('!'),width=8,height=2)
-root = tkinter.Button(win,text="√x",command=executables.print_n_root,width=8,height=2)
-tpow = tkinter.Button(win,text="x^n",command=lambda: executables.print_num('^'),width=8,height=2)
-mod = tkinter.Button(win,text="%",command=lambda: executables.print_num('%'),width=8,height=2)
+fac = tkinter.Button(win,text="n!",command=lambda: executables.print_num('!'),width=5,height=2)
+root = tkinter.Button(win,text="n√x",command=executables.print_n_root,width=5,height=2)
+tpow = tkinter.Button(win,text="x^n",command=lambda: executables.print_num('^'),width=5,height=2)
+mod = tkinter.Button(win,text="%",command=lambda: executables.print_num('%'),width=5,height=2)
 
-equals = tkinter.Button(win,text="=",command=executables.execute,width=18,height=5)
+equals = tkinter.Button(win,text="=",command=executables.execute,width=14,height=5)
 
 #binding tooltips to buttons and setting messages
-tooltip_plus.bind_widget(plus, balloonmsg="place inbetween 2 expressions for addition")
-tooltip_minus.bind_widget(minus, balloonmsg="place inbetween 2 expressions for substraction")
-tooltip_mul.bind_widget(times, balloonmsg="place inbetween 2 expressions for multiplication")
-tooltip_div.bind_widget(division, balloonmsg="place inbetween 2 expressions for division")
-tooltip_del.bind_widget(delete, balloonmsg="deletes last character")
-tooltip_ce.bind_widget(ce, balloonmsg="clears all")
-tooltip_fac.bind_widget(fac, balloonmsg="place after an expression for factorial")
-tooltip_root.bind_widget(root, balloonmsg="place before an expression for root of n")
-tooltip_pow.bind_widget(tpow, balloonmsg="place after an expression for pow to the power of n")
+#tooltip_plus.bind_widget(plus, balloonmsg="place inbetween 2 expressions for addition")
+#tooltip_minus.bind_widget(minus, balloonmsg="place inbetween 2 expressions for substraction")
+#tooltip_mul.bind_widget(times, balloonmsg="place inbetween 2 expressions for multiplication")
+#tooltip_div.bind_widget(division, balloonmsg="place inbetween 2 expressions for division")
+#tooltip_del.bind_widget(delete, balloonmsg="deletes last character")
+#tooltip_ce.bind_widget(ce, balloonmsg="clears all")
+#tooltip_fac.bind_widget(fac, balloonmsg="place after an expression for factorial")
+#tooltip_root.bind_widget(root, balloonmsg="place before an expression for root of n")
+#tooltip_pow.bind_widget(tpow, balloonmsg="place after an expression for pow to the power of n")
 
 
 #placing buttons
 
-display.place(x=19,y=20)
+display.place(x=19,y=30)
 
-num1.place(x=20,y=350)
-num4.place(x=20,y=300)
-num7.place(x=20,y=250)
-num2.place(x=90,y=350)
-num5.place(x=90,y=300)
-num8.place(x=90,y=250)
-num3.place(x=160,y=350)
-num6.place(x=160,y=300)
-num9.place(x=160,y=250)
+num1.place(x=20,y=360)
+num4.place(x=20,y=310)
+num7.place(x=20,y=260)
+num2.place(x=90,y=360)
+num5.place(x=90,y=310)
+num8.place(x=90,y=260)
+num3.place(x=160,y=360)
+num6.place(x=160,y=310)
+num9.place(x=160,y=260)
 
-fac.place(x=20,y=200)
-root.place(x=90,y=200)
-tpow.place(x=160,y=200)
-mod.place(x=160,y=400)
+fac.place(x=20,y=210)
+root.place(x=90,y=210)
+tpow.place(x=160,y=210)
+mod.place(x=160,y=410)
 
-plus.place(x=300,y=300)
-minus.place(x=230,y=300)
-times.place(x=230,y=250)
+plus.place(x=300,y=310)
+minus.place(x=230,y=310)
+times.place(x=230,y=260)
 
-num0.place(x=20,y=400)
-float_point.place(x=90,y=400)
-delete.place(x=230,y=200)
-ce.place(x=300,y=200)
-equals.place(x=230,y=352)
-division.place(x=300,y=250)
+num0.place(x=20,y=410)
+float_point.place(x=90,y=410)
+delete.place(x=230,y=210)
+ce.place(x=300,y=210)
+equals.place(x=230,y=362)
+division.place(x=300,y=260)
 
+help_in.place(x=0,y=0)
 # binding keyboard to buttons
 win.bind("1", lambda event:executables.print_num('1'))
 win.bind("2", lambda event:executables.print_num('2'))
@@ -140,5 +143,6 @@ win.bind("!", lambda event:executables.print_num('!'))
 win.bind("%", lambda event:executables.print_num('%'))
 win.bind("^", lambda event:executables.print_num('^'))
 win.bind(".", lambda event:executables.print_num('.'))
+
 
 win.mainloop()
